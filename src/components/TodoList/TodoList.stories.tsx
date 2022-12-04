540f22db5347b2e9d22a72ef72f40677f5a53c45
ruritoBlogger <ruriro0125@gmail.com>
2022-12-04 18:45:17 +0900

    :sparkles: TodoListの失敗時などのStoryを追加

--- src/components/TodoList/TodoList.stories.tsx
+++ src/components/TodoList/TodoList.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 import { TodoList } from "./TodoList";
 import {todosQueryHandler} from "./todosQueryHandler";
+import {disableCacheDecorator} from "../../utils/customStoryDecorator";
 
 export default {
   component: TodoList,
@@ -14,4 +15,26 @@ export const Default: ComponentStoryObj<typeof TodoList> = {
       handlers: [todosQueryHandler({})]
     },
   },
+};
+
+export const onError: ComponentStoryObj<typeof TodoList> = {
+  args: {},
+  name: "データ取得に失敗時",
+  parameters: {
+    msw: {
+      handlers: [todosQueryHandler({errors: "エラー！！！"})]
+    },
+  },
+  decorators: [disableCacheDecorator]
+};
+
+export const Loading: ComponentStoryObj<typeof TodoList> = {
+  args: {},
+  name: "データ取得中時",
+  parameters: {
+    msw: {
+      handlers: [todosQueryHandler({loading: true})]
+    },
+  },
+  decorators: [disableCacheDecorator]
 };
